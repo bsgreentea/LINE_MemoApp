936a6ddf7f84538d7fb3986eaf36cc03a38debe5
bsgreentea <46317084+bsgreentea@users.noreply.github.com>
2020-02-17 19:36:11 +0900

    Create Memo.kt
    Create memo class

---
+++ app/src/main/java/com/greentea/line_memoapp/Model/Memo.kt
@@ -0,0 +1,14 @@
+package com.greentea.line_memoapp.Model
+
+import android.media.Image
+import androidx.room.*
+
+@Entity(tableName = "memo_table")
+class Memo(
+    @PrimaryKey(autoGenerate = true) var memoId: Int,
+    @ColumnInfo(name = "memoTitle") var memoTitle: String,
+    @ColumnInfo(name = "memoContent") var memoContent: String,
+    @ColumnInfo(name = "images")
+    @TypeConverters(ImagesConverter::class)
+    var Imgs: List<Image>
+)
