1e14f2efc002b0f7b54170b092e01cd28d39e9a2
bsgreentea <46317084+bsgreentea@users.noreply.github.com>
2020-02-19 00:09:12 +0900

    Create MemoViewModel.kt

---
+++ app/src/main/java/com/greentea/line_memoapp/ViewModel/MemoViewModel.kt
@@ -0,0 +1,27 @@
+package com.greentea.line_memoapp.ViewModel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.greentea.line_memoapp.AppDB.AppDB
+import com.greentea.line_memoapp.AppDB.MemoRepository
+import com.greentea.line_memoapp.Model.Memo
+import kotlinx.coroutines.launch
+
+class MemoViewModel(application: Application): AndroidViewModel(application){
+
+    private val repository: MemoRepository
+
+    val allMemos: LiveData<List<Memo>>
+
+    init{
+        val memoDAO = AppDB.getDB(application).memoDAO()
+        repository = MemoRepository(memoDAO)
+        allMemos = repository.allMemos
+    }
+
+    fun insert(memo: Memo) = viewModelScope.launch{
+        repository.insert(memo)
+    }
+}
