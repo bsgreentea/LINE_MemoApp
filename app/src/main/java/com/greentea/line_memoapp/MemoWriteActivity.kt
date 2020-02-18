e83e2aa7bf969e01ce4e11169f968f3831ae7122
bsgreentea <46317084+bsgreentea@users.noreply.github.com>
2020-02-18 20:17:04 +0900

    Create MemoWriteActivity.kt
    메모 작성 페이지 구현하기

---
+++ app/src/main/java/com/greentea/line_memoapp/MemoWriteActivity.kt
@@ -0,0 +1,12 @@
+package com.greentea.line_memoapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MemoWriteActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_memo_write)
+    }
+}
