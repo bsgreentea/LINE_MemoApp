8feaa7d90039cb66b2b44b75e820ec56b6d08a28
bsgreentea <46317084+bsgreentea@users.noreply.github.com>
2020-02-18 20:14:27 +0900

    Update Memo.kt
    이미지 저장하는 방법 생각해보기

--- app/src/main/java/com/greentea/line_memoapp/Model/Memo.kt
+++ app/src/main/java/com/greentea/line_memoapp/Model/Memo.kt
@@ -1,14 +1,15 @@
 package com.greentea.line_memoapp.Model
 
 import android.media.Image
+import android.provider.MediaStore
 import androidx.room.*
 
 @Entity(tableName = "memo_table")
 class Memo(
     @PrimaryKey(autoGenerate = true) var memoId: Int,
     @ColumnInfo(name = "memoTitle") var memoTitle: String,
-    @ColumnInfo(name = "memoContent") var memoContent: String,
-    @ColumnInfo(name = "images")
-    @TypeConverters(ImagesConverter::class)
-    var Imgs: List<Image>
+    @ColumnInfo(name = "memoContent") var memoContent: String//,
+//    @ColumnInfo(name = "images")
+//    @TypeConverters(ImagesConverter::class)
+//    var Imgs: List<MediaStore.Images>
 )
