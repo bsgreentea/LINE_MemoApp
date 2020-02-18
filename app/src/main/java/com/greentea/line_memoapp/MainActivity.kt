3ec7eb4faec3a798b45486256b3da86153a3830f
bsgreentea <46317084+bsgreentea@users.noreply.github.com>
2020-02-18 20:15:16 +0900

    Update MainActivity.kt
    리사이클러뷰 화면에 나오는 것 확인

--- app/src/main/java/com/greentea/line_memoapp/MainActivity.kt
+++ app/src/main/java/com/greentea/line_memoapp/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.greentea.line_memoapp
 
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.LayoutInflater
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.greentea.line_memoapp.Adapter.MemoViewAdapter
+import com.greentea.line_memoapp.Model.Memo
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var temp = arrayListOf<Memo>()
+        temp.add(Memo(0, "title1", "content1"))
+        temp.add(Memo(0, "title2", "content2"))
+
+        val adapter = MemoViewAdapter(this, temp)
+        var recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
+        recyclerView.adapter = adapter
+
+        val lm = LinearLayoutManager(this)
+        recyclerView.layoutManager = lm
     }
 }
