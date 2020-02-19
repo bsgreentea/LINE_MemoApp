47ffd0900738ce561a4d84d9ccb34b1af88ba6b7
bsgreentea <46317084+bsgreentea@users.noreply.github.com>
2020-02-19 23:24:20 +0900

    Update Activities
    MainActivity
    메모 작성 버튼을 누르면 새 창이 뜨게끔 구현함
    
    MemoWriteActivity
    제목과 내용을 입력할 수 있음
    저장 버튼 클릭시 제목과 내용 정보를 담은 intent 반환

--- app/src/main/java/com/greentea/line_memoapp/MainActivity.kt
+++ app/src/main/java/com/greentea/line_memoapp/MainActivity.kt
@@ -1,30 +1,64 @@
 package com.greentea.line_memoapp
 
+import android.app.Activity
+import android.content.Intent
 import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.LayoutInflater
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.greentea.line_memoapp.Adapter.MemoViewAdapter
 import com.greentea.line_memoapp.Model.Memo
+import com.greentea.line_memoapp.ViewModel.MemoViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    val NEW_MEMO_REQUEST_CODE = 1
+    lateinit var addButton: FloatingActionButton
+    lateinit var memoViewModel: MemoViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var temp = arrayListOf<Memo>()
-        temp.add(Memo(0, "title1", "content1"))
-        temp.add(Memo(0, "title2", "content2"))
+        init()
+    }
 
-        val adapter = MemoViewAdapter(this, temp)
+    fun init(){
+
+        val adapter = MemoViewAdapter(this)
         var recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
         recyclerView.adapter = adapter
 
         val lm = LinearLayoutManager(this)
         recyclerView.layoutManager = lm
+
+        memoViewModel = ViewModelProvider(this).get(MemoViewModel::class.java)
+        memoViewModel.allMemos.observe(this, Observer { memos->
+            memos?.let{adapter.setMemos(it)}
+        })
+
+        addButton = findViewById(R.id.fab)
+        addButton.setOnClickListener{
+            val intent = Intent(this@MainActivity, MemoWriteActivity::class.java)
+            startActivityForResult(intent, NEW_MEMO_REQUEST_CODE)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == NEW_MEMO_REQUEST_CODE && resultCode == Activity.RESULT_OK){
+            val title = data!!.getStringExtra("title").toString()
+            val content = data!!.getStringExtra("content").toString()
+            val memo = Memo(0,title,content)
+
+            memoViewModel.insert(memo)
+        }
     }
 }
--- app/src/main/java/com/greentea/line_memoapp/MemoWriteActivity.kt
+++ app/src/main/java/com/greentea/line_memoapp/MemoWriteActivity.kt
@@ -1,12 +1,45 @@
 package com.greentea.line_memoapp
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import org.w3c.dom.Text
 
 class MemoWriteActivity : AppCompatActivity() {
+
+    lateinit var editTitle: TextView
+    lateinit var editContent: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_memo_write)
+
+        editTitle = findViewById(R.id.edit_title)
+        editContent = findViewById(R.id.edit_contents)
+
+        val saveBtn = findViewById<Button>(R.id.save_btn)
+        saveBtn.setOnClickListener {
+
+            val replyIntent = Intent()
+            if(TextUtils.isEmpty(editTitle.text)){
+                Toast.makeText(this, "제목을 입력하세요.",Toast.LENGTH_SHORT).show()
+            } else {
+
+                val title = editTitle.text.toString()
+                val content = editContent.text.toString()
+
+                replyIntent.putExtra("title", title)
+                replyIntent.putExtra("content", content)
+
+                setResult(Activity.RESULT_OK, replyIntent)
+
+                finish()
+            }
+        }
     }
 }
